5cb766da1f58951831ac95d9b9e7466abe083bf8
System Administrator <root@wifi-64-187-249-23.nyit.edu>
2024-08-07 18:45:54 -0400

    aichatsupprt update

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
     ])
   
     try {
-      const response = await fetch('/api/chat', {
+      const response = await fetch('./api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
